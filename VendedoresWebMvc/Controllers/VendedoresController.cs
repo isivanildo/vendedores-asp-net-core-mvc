50ed4071b57c8f0ab2be81e7d141a6957ffe7200
Ivanildo Ferreira <isivanildo@hotmail.com>
2021-12-22 17:25:43 -0300

    Criando formulário simples

--- VendedoresWebMvc/Controllers/VendedoresController.cs
+++ VendedoresWebMvc/Controllers/VendedoresController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VendedoresWebMvc.Models;
 using VendedoresWebMvc.Services;
 
 namespace VendedoresWebMvc.Controllers
@@ -19,6 +20,20 @@ namespace VendedoresWebMvc.Controllers
         {
             var lista = _vendedor.FindAll();
             return View(lista);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Vendedor vendedor)
+        {
+            _vendedor.Inserir(vendedor);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
